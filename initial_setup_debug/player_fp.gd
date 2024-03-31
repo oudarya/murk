db662181a944a8ea5954328bccaf39e0ff99b626
Oudarya Ghosh <unofficiallyoudarya@gmail.com>
2024-04-01 01:40:42 +0530

    player basic camera movement implemented

--- initial_setup_debug/player_fp.gd
+++ initial_setup_debug/player_fp.gd
@@ -1,10 +1,22 @@
 extends CharacterBody3D
 
 
-var speed = 5.0
-var jump_velocity = 4.5
-var gravity = 9.8
+var speed : float = 5.0
+var jump_velocity : float = 4.5
+var gravity : float = 9.8
+var mouse_sensitivity : float = 0.015
 
+@onready var head : Node3D = $head
+@onready var player_camera : Camera3D = $head/player_camera
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * mouse_sensitivity)
+		player_camera.rotate_x(-event.relative.y * mouse_sensitivity)
+		player_camera.rotation.x = clamp(player_camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -23,7 +35,7 @@ func _physics_process(delta):
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.z = move_toward(velocity.z, 0, speed)
+		velocity.x = 0.0
+		velocity.z = 0.0
 
 	move_and_slide()
